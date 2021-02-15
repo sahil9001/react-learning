493ed56ff550839cf7583f9445592c03f9cded8f
sahil9001 <sahilsilare@gmail.com>
2021-02-15 17:05:47 +0530

    learn: Passing data to functions

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Component } from "react";
 
-class Body extends Component {
-  render() {
-    return <p>{this.props.title}</p>;
-  }
+function Body(props) {
+  return (
+    <div>
+      <p>
+        {props.title} and {props.num}
+      </p>
+      <p>{props.myObj.b}</p>
+      <p>{props.myArr[0]}</p>
+    </div>
+  );
 }
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Body title="test title" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Body
+          title="test title"
+          num={5}
+          myObj={{
+            a: 5,
+            b: 6,
+          }}
+          myArr={[23, 5, 34, 2]}
+        />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
